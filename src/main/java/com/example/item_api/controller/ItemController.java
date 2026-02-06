cbf6254620dc275523b870c81a90e78aa9475210
mugunthanm2k <mugunthanm2k@gmail.com>
2026-02-06 17:05:43 +0530

    Add CORSS

--- src/main/java/com/example/item_api/controller/ItemController.java
+++ src/main/java/com/example/item_api/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.example.item_api.service.ItemService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("api/items")
 @Validated
 public class ItemController {
